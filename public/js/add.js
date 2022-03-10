aca849d700da8e37c2ad38af17b8f023c6993903
Glitch (romantic-open-notebook) <none>
2022-03-10 16:02:02 +0000

    🏮🎎 Checkpoint ./public/js/add.js:30475187/1800

--- public/js/add.js
+++ public/js/add.js
@@ -1,14 +1,20 @@
 let firstNumber = Math.floor(Math.random()*100);
+let fNS = firstNumber.toString();
 let secondNumber = Math.floor(Math.random()*firstNumber);
+let sNS = secondNumber.toString();
 let answer = firstNumber + secondNumber;
 
 while (firstNumber + secondNumber > 99 || 
-    fNS.length != 2 ||
-    sNS.length != 2) {
-    firstNumber = Math.floor(Math.random()*100);
-    secondNumber = Math.floor(Math.random()*firstNumber);
-    answer = firstNumber + secondNumber;
+  fNS.length != 2 ||
+  sNS.length != 2) {
+  firstNumber = Math.floor(Math.random()*100);
+  fNS = firstNumber.toString();
+  secondNumber = Math.floor(Math.random()*firstNumber);
+  sNS = secondNumber.toString();
+  answer = firstNumber + secondNumber;
 }
-let fNS = firstNumber.toString();
-let sNS = secondNumber.toString();
 
+// Show numbers
+function showNumbers() {
+  
+}
