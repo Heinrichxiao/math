adabf7aaea709a227230bba44b60884444f51b1e
Glitch (romantic-open-notebook) <none>
2022-03-10 13:23:31 +0000

    🔮🌶 Checkpoint ./index.js:30475187/2519 ./views/play.pug:30475187/1 ./package.json:30475187/197 ./db.json:30475187/27

--- index.js
+++ index.js
@@ -1,51 +1,47 @@
 module.exports = () => {
-    require('dotenv').config();
-  
-    const express = require('express');
-    const app = express();
-    const port = process.env.PORT || 8080;
-    const path = require('path');
-    const bodyParser = require('body-parser');
-    const session = require('express-session');
-    const login = require('./routes/login');
-    const signup = require('./routes/signup');
-    const record = require('./routes/record');
-    const logout = require('./routes/logout');
-    const play = require('./routes/play');
-    const index = require('./routes/index');
-    const renameDb = require('./helpers/renameDb');
+  require("dotenv").config();
 
-    // Rename database
-    renameDb();
-    
-    // Set views
-    app.set('view engine', 'pug');
-    app.set('views', path.join(__dirname, './views'));
+  const express = require("express");
+  const app = express();
+  const port = process.env.PORT || 8080;
+  const path = require("path");
+  const bodyParser = require("body-parser");
+  const session = require("express-session");
+  const login = require("./routes/login");
+  const signup = require("./routes/signup");
+  const logout = require("./routes/logout");
+  const index = require("./routes/index");
+  const renameDb = require("./helpers/renameDb");
 
-    // Use libraries or features
-    app.use(bodyParser());
-    app.use(
-        session({
-            resave: false,
-            saveUninitialized: false,
-            secret: process.env.SESSION_SECRET
-        })
-    );
-    app.use(express.static('public'));
-    
-    // Use routers
-    app.use(signup);
-    app.use(login);
-    app.use(index);
-    app.use(logout);
-    app.use(record);
-    app.use(play);
-  
-    app.get('/*', (req, res) => {
-        res.render('404');
-    });
-  
-    app.listen(port, () => {
-        console.log(`Starting server at: https://localhost:${port}`);
-    });
+  // Rename database
+  renameDb();
+
+  // Set views
+  app.set("view engine", "pug");
+  app.set("views", path.join(__dirname, "./views"));
+
+  // Use libraries or features
+  app.use(bodyParser());
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.SESSION_SECRET,
+    })
+  );
+  app.use(express.static("public"));
+
+  // Use routers
+  app.use(signup);
+  app.use(login);
+  app.use(index);
+  app.use(logout);
+
+  app.get("/*", (req, res) => {
+    res.render("404");
+  });
+
+  app.listen(port, () => {
+    console.log(`Starting server at: https://localhost:${port}`);
+  });
 };
