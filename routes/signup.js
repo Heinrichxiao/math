8ce7650a688bba161c32e0059a8f80f8b167a9cc
Glitch (romantic-open-notebook) <none>
2022-03-10 13:34:42 +0000

    📒🍠 Checkpoint ./routes/signup.js:30475187/2041

--- routes/signup.js
+++ routes/signup.js
@@ -1,44 +1,48 @@
-const express = require('express');
+const express = require("express");
 const signup = express.Router();
-const hash = require('../hash');
-const db = require('../db.json');
-const save = require('../save');
+const hash = require("../hash");
+const db = require("../db.json");
+const save = require("../save");
 
-signup.get('/signup', (req, res) => {
-    if (req.session.userid) {
-        res.redirect('/');
-        return;
-    }
-    res.render('signup');
+signup.get("/signup", (req, res) => {
+  if (req.session.userid) {
+    res.redirect("/");
     return;
+  }
+  res.render("signup");
+  return;
 });
 
-signup.post('/signup', (req, res) => {
-    if (req.session.userid) {
-        res.redirect('/');
-        return;
-    }
+signup.post("/signup", (req, res) => {
+  if (req.session.userid) {
+    res.redirect("/");
+    return;
+  }
 
-    if (req.body.usr == '') {
-        res.redirect('/signup');
-        return;
-    }
-    if (req.body.psw == '') {
-        res.redirect('/signup');
-        return;
-    }
-    if (Object.keys(db.users).includes(req.body.usr)) {
-        res.redirect('/login');
-        return;
-    }
-    db.users[req.body.usr] = {
-        usr: req.body.usr,
-        psw: hash(req.body.psw)
-    };
+  if (req.body.usr == "") {
+    res.redirect("/signup");
+    return;
+  }
+  if (req.body.psw == "") {
+    res.redirect("/signup");
+    return;
+  }
+  if (req.body.psw != req.body.pswCon) {
+    res.redirect("/signup");
+    return;
+  }
+  if (Object.keys(db.users).includes(req.body.usr)) {
+    res.redirect("/login");
+    return;
+  }
+  db.users[req.body.usr] = {
+    usr: req.body.usr,
+    psw: hash(req.body.psw),
+  };
 
-    save(db);
+  save(db);
 
-    res.redirect('/login');
+  res.redirect("/login");
 });
 
 module.exports = signup;
